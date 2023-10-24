5fddd4534a7cdaece27a04ca594a9d8d1d9161df
NickNameIsSSS <tdp280505@gmail.com>
2023-10-24 23:01:26 +0700

    Thử nghiệm Git và Github

---
+++ text - Copy.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b
+    printf("Nhap A va B:");
+    scanf("%d %d", &a, &b);
+    printf("\nTong cua %d va %d la: %d", a, b, a + b);
+    return 0;
+}
